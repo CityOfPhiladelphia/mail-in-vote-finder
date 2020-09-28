5dd91d21ea5f7af418e7d82833ddd43baf521fac
Andy Rothwell <rothwell.andy@gmail.com>
2020-09-28 11:51:37 -0400

    removes bad link from footer

--- src/main.js
+++ src/main.js
@@ -136,12 +136,12 @@ pinboard({
   },
   footer: {
     'HowToUse': false,
-    'OtherLinks': {
-      locations: {
-        text: 'View accessible list of site locations',
-        link: 'https://www.phila.gov/programs/access-centers/access-center-sites/#/',
-      },
-    },
+    // 'OtherLinks': {
+    //   locations: {
+    //     text: 'View accessible list of site locations',
+    //     link: 'https://www.phila.gov/programs/access-centers/access-center-sites/#/',
+    //   },
+    // },
   },
   // infoCircles: {
   //   'symptomatic': {
