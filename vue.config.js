71e4ec2177f524aa5a4f016c8fe752798903337f
Andy Rothwell <rothwell.andy@gmail.com>
2020-09-29 09:55:23 -0400

    fixes public path again in dev

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   filenameHashing: false,
-  publicPath: "/voting/dev/",
+  publicPath: "/voting/",
   //publicPath: "/access-centers/dev/",
   chainWebpack: (config) => {
     config.resolve.symlinks(false);
