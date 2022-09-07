968776f4380ab140c018f905364530d9c379da6a
Andy Rothwell <Andy.Rothwell@phila.gov>
2022-09-07 13:35:17 -0400

    uses pinboard 1.1.36

--- src/components/ExpandCollapseContent.vue
+++ src/components/ExpandCollapseContent.vue
@@ -1,31 +1,36 @@
 <template>
-  <div>
-    <div class="columns is-marginless">
-      <div class="column">
+  <div :class="isMobile ? 'main-content-mobile' : 'main-content'">
+    <div class="columns">
+      <div class="column is-6">
         <div
           v-if="item.street_address"
-          class="detail"
+          class="columns is-mobile"
         >
-          <font-awesome-icon icon="map-marker-alt" />
-          <span>
+          <div class="column is-1">
+            <font-awesome-icon icon="map-marker-alt" />
+          </div>
+
+          <div class="column is-11">
             {{ item.street_address }}<br>
             Philadelphia, PA {{ item.zip }}<br>
-          </span>
+          </div>
         </div>
       </div>
 
       <div
         v-if="section !== 'Official mobile mail-in ballot return'"
-        class="column"
+        class="column is-6"
       >
         <div
           v-if="item.phone_number"
-          class="detail"
+          class="columns is-mobile"
         >
-          <font-awesome-icon icon="phone" />
-          <span>
+          <div class="column is-1">
+            <font-awesome-icon icon="phone" />
+          </div>
+          <div class="column is-11">
             {{ item.phone_number }}
-          </span>
+          </div>
         </div>
       </div>
     </div>
@@ -81,59 +86,59 @@ export default {
 
 <style lang="scss">
 
-table {
-  border: 0px none;
-  border-style: none !important;
-}
-
-.table-intro {
-  margin-bottom: 16px;
-}
-
-.vgt-inner-wrap {
-  box-shadow: 0 0 0 0;
-}
-
-.vgt-table th {
-  padding: 0.4em 0.4em 0.4em 0.4em !important;
-  border-bottom-color: #444444 !important;
-  border-bottom-width: 2px !important;
-  background: #ffffff !important;
-  text-align: left;
-}
-
-table td:not([align]), table th:not([align]) {
-  text-align: left !important;
-}
-
-.vgt-table td {
-  // border-bottom-color: #444444 !important;
-}
-
-.vgt-table tr:nth-child(odd) {
-  background: #eee;
-}
-
-.center {
-  text-align: center;
-}
-
-.table-text {
-  font-family: "OpenSans-Regular", "Open Sans", sans-serif;
-  font-size: 14px;
-}
-
-.table-header-text {
-  font-family: "OpenSans-SemiBold", "Open Sans SemiBold", "Open Sans", sans-serif;
-  font-size: 14px;
-}
-
-.no-margin {
-  margin: 0px;
-}
-
-.no-header {
-  display: none;
-}
+// table {
+//   border: 0px none;
+//   border-style: none !important;
+// }
+//
+// .table-intro {
+//   margin-bottom: 16px;
+// }
+//
+// .vgt-inner-wrap {
+//   box-shadow: 0 0 0 0;
+// }
+//
+// .vgt-table th {
+//   padding: 0.4em 0.4em 0.4em 0.4em !important;
+//   border-bottom-color: #444444 !important;
+//   border-bottom-width: 2px !important;
+//   background: #ffffff !important;
+//   text-align: left;
+// }
+//
+// table td:not([align]), table th:not([align]) {
+//   text-align: left !important;
+// }
+//
+// .vgt-table td {
+//   // border-bottom-color: #444444 !important;
+// }
+//
+// .vgt-table tr:nth-child(odd) {
+//   background: #eee;
+// }
+//
+// .center {
+//   text-align: center;
+// }
+//
+// .table-text {
+//   font-family: "OpenSans-Regular", "Open Sans", sans-serif;
+//   font-size: 14px;
+// }
+//
+// .table-header-text {
+//   font-family: "OpenSans-SemiBold", "Open Sans SemiBold", "Open Sans", sans-serif;
+//   font-size: 14px;
+// }
+//
+// .no-margin {
+//   margin: 0px;
+// }
+//
+// .no-header {
+//   display: none;
+// }
 
 </style>
