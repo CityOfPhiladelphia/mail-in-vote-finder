00d8a4198a84fbbf3ee853aab21760a37e215649
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-08-06 12:55:54 -0400

    fix election dates for fall 2025

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -60,7 +60,7 @@ export default {
     'Election office': {
       header: 'Election offices',
       englishName: 'Election office',
-      p1: 'Beginning May 5, 2025, the Philadelphia Board of Elections will be open for in-person mail-in ballot services for the May 20, 2025 Municipal Primary Election. Registered voters can apply for, receive, complete, and return their mail-in ballot in person in one visit.',
+      p1: 'Beginning October 28, 2025, the Philadelphia Board of Elections will be open for in-person mail-in ballot services for the November 4, 2025 Municipal Primary Election. Registered voters can apply for, receive, complete, and return their mail-in ballot in person in one visit.',
       h2: 'Voter services and deadlines ',
       dates: {
         // d1:{
@@ -69,15 +69,15 @@ export default {
         // },
         d2:{
           text: 'Apply for a mail-in ballot',
-          date: 'May 13 by 5:00 p.m.',
+          date: 'October 28 by 5:00 p.m.',
         },
         d3:{
           text: 'Request a replacement ballot',
-          date: 'No earlier than May 5',
+          date: 'TBD',
         },
         d4:{
           text: 'Drop off your completed ballot',
-          date: 'May 20 by 8:00 p.m.',
+          date: 'November 4, by 8:00 p.m.',
         },
       },
       // callout1: 'Masks are required regardless of vaccination status.',
