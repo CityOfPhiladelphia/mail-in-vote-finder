673aed8bdd1e6e40dcb87c46c5d36ee8414b6817
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-08-26 15:19:18 -0400

    makes project work with @phila/auto-translator-cli

--- src/i18n/ch.js
+++ src/i18n/ch.js
@@ -1,121 +1,121 @@
-export default {
-  language: "中文",
-  app: {
-    title: "選舉辦公室和郵寄選票投遞地點",
-    subtitle: "查看您附近的地點",
-    noResults: "No site was found within your search. Please try again."
+export default{
+  "language": "中文",
+  "app": {
+    "title": "選舉辦公室和郵寄選票投遞地點",
+    "subtitle": "查看您附近的地點",
+    "noResults": "No site was found within your search. Please try again."
   },
-  introPage: {
-    exclamation: "此地图包含的投票点仅受理邮寄选票和缺席选票。如需现场投票，请在 atlas.phila.gov/voting 网站上查找投票点。",
-    introTitle: "关于此搜索工具",
-    p1_1: "除现有的两处县选举委员会办公室外，费城市政专员已批准位于费城市内的",
-    p1_2: " 处卫星选举办公室。",
-    p2: "仅到访这些地点一次，选民便可以：",
-    ul1: {
-      li1: "登记投票、",
-      li2: "申请邮寄选票、",
-      li3: "领取选票、投票并返回选票。"
+  "introPage": {
+    "exclamation": "此地图包含的投票点仅受理邮寄选票和缺席选票。如需现场投票，请在 atlas.phila.gov/voting 网站上查找投票点。",
+    "introTitle": "关于此搜索工具",
+    "p1_1": "除现有的两处县选举委员会办公室外，费城市政专员已批准位于费城市内的",
+    "p1_2": " 处卫星选举办公室。",
+    "p2": "仅到访这些地点一次，选民便可以：",
+    "ul1": {
+      "li1": "登记投票、",
+      "li2": "申请邮寄选票、",
+      "li3": "领取选票、投票并返回选票。"
     },
-    p3: "选民也可以在选举办公室提交其完成的选票。",
-    p4_b: "注意： ",
-    p4: "选民必须等到 10 月 6 日才能在办公室申请替换选票。这将防止邮件中的选票重复。",
-    relatedContent: "相关内容",
-    link1: "查看您的选民登记状态",
-    link2: "查看您的邮寄选票状态",
-    link3: "参阅我们的费城选民邮寄选票指南"
+    "p3": "选民也可以在选举办公室提交其完成的选票。",
+    "p4_b": "注意： ",
+    "p4": "选民必须等到 10 月 6 日才能在办公室申请替换选票。这将防止邮件中的选票重复。",
+    "relatedContent": "相关内容",
+    "link1": "查看您的选民登记状态",
+    "link2": "查看您的邮寄选票状态",
+    "link3": "参阅我们的费城选民邮寄选票指南"
   },
-  cityOfPhiladelphia: "费城市",
-  cityCommissioners: "市政专员",
-  ballotGuide: "大选邮寄选票指南",
-  siteHours: "站点营业时间",
-  details: {
-    details: "详情",
-    inPerson: "亲自登记和邮寄投票",
-    voterReg: " 选民登记",
-    ballotApp: "邮寄选票申请",
-    ballotRep: "邮寄选票替代 ",
-    ballotDropoff: "邮寄选票提交",
-    ballotDropoffMobile: "移動式郵寄選票投遞",
-    ballotDropoffMobileStandAlone: "<div class='callout'>這個不是固定獨立的投遞箱。</div>",
-    interpretationAvailable: "提供电话口译服务",
-    wheelchair: "轮椅无障碍服务",
-    open24Hours: "24 小时开放"
+  "cityOfPhiladelphia": "费城市",
+  "cityCommissioners": "市政专员",
+  "ballotGuide": "大选邮寄选票指南",
+  "siteHours": "站点营业时间",
+  "details": {
+    "details": "详情",
+    "inPerson": "亲自登记和邮寄投票",
+    "voterReg": " 选民登记",
+    "ballotApp": "邮寄选票申请",
+    "ballotRep": "邮寄选票替代 ",
+    "ballotDropoff": "邮寄选票提交",
+    "ballotDropoffMobile": "移動式郵寄選票投遞",
+    "ballotDropoffMobileStandAlone": "<div class='callout'>這個不是固定獨立的投遞箱。</div>",
+    "interpretationAvailable": "提供电话口译服务",
+    "wheelchair": "轮椅无障碍服务",
+    "open24Hours": "24 小时开放"
   },
   "Election office": "选举办公室",
   "Official mobile mail-in ballot return": "正式移動式郵寄选票投遞処",
   "Official mail-in ballot dropbox": "官方邮寄选票投票箱",
-  categoryType: {
+  "categoryType": {
     "Election office": "选举办公室",
     "Official mobile mail-in ballot return": "正式移動式郵寄选票投遞処",
     "Official mail-in ballot dropbox": "官方邮寄选票投票箱",
     "Mail-in ballot dropbox (temporarily closed)": "邮寄投票箱（暂时关闭）"
   },
-  sections: {
+  "sections": {
     "Election office": {
-      header: "选举办公室",
-      englishName: "Election office",
-      p1: "從2025年10月28日起，費城選舉委員會將開放2025年11月4日市政初選的面對面郵寄選票服務。已註冊的選民可以在一次造訪中親自申請、接收、填寫並交回郵寄選票。",
-      h2: "选民服务及截止日期",
-      dates: {
-        d2:{
-          text: "申請郵寄選票的截至日期",
-          date: "10 月 28 日下午 5:00 之前"
+      "header": "选举办公室",
+      "englishName": "Election office",
+      "p1": "從2025年10月28日起，費城選舉委員會將開放2025年11月4日市政初選的面對面郵寄選票服務。已註冊的選民可以在一次造訪中親自申請、接收、填寫並交回郵寄選票。",
+      "h2": "选民服务及截止日期",
+      "dates": {
+        "d2": {
+          "text": "申請郵寄選票的截至日期",
+          "date": "10 月 28 日下午 5:00 之前"
         },
-        d3:{
-          text: "申請補發選票的截至日期",
-          date: "TBD"
+        "d3": {
+          "text": "申請補發選票的截至日期",
+          "date": "TBD"
         },
-        d4:{
-          text: "投递您填好的选票的截至日期",
-          date: "11 月 4 日晚上 8:00 之前"
+        "d4": {
+          "text": "投递您填好的选票的截至日期",
+          "date": "11 月 4 日晚上 8:00 之前"
         }
       }
     },
     "Official mobile mail-in ballot return": {
-      header: "正式选票寄还地点",
-      englishName: "Official mobile mail-in ballot return",
-      p1: "移動式郵寄選票投遞箱",
-      h2: "",
-      dates: "",
-      callout1: "這不是一個固定的獨立投遞箱。投遞時間有限。",
-      callout2: ""
+      "header": "正式选票寄还地点",
+      "englishName": "Official mobile mail-in ballot return",
+      "p1": "移動式郵寄選票投遞箱",
+      "h2": "",
+      "dates": "",
+      "callout1": "這不是一個固定的獨立投遞箱。投遞時間有限。",
+      "callout2": ""
     },
     "Official mail-in ballot dropbox": {
-      header: "官方邮寄选票投票箱",
-      englishName: "Official mail-in ballot dropbox",
-      p1: "如果投票箱满了、故障或以任何形式损坏，请致电 (215) 686-3469 或发送电子邮件至 <a href='mailto:vote{'@'}phila.gov'>vote{'@'}phila.gov</a>.",
-      h2: "",
-      dates: "",
-      callout1: "投票箱将于选举当日晚8点准时关闭。",
-      callout2: ""
+      "header": "官方邮寄选票投票箱",
+      "englishName": "Official mail-in ballot dropbox",
+      "p1": "如果投票箱满了、故障或以任何形式损坏，请致电 (215) 686-3469 或发送电子邮件至 <a href='mailto:vote{'@'}phila.gov'>vote{'@'}phila.gov</a>.",
+      "h2": "",
+      "dates": "",
+      "callout1": "投票箱将于选举当日晚8点准时关闭。",
+      "callout2": ""
     },
     "Mail-in ballot dropbox (temporarily closed)": {
-      header: "邮寄投票箱（暂时关闭）",
-      englishName: "Mail-in ballot dropbox (temporarily closed)",
-      p1: "如果投票箱满了、故障或以任何形式损坏，请致电 (215) 686-3469 或发送电子邮件至 <a href='mailto:vote{'@'}phila.gov'>vote{'@'}phila.gov</a>.",
-      h2: "",
-      dates: "",
-      callout1: "投票箱将于选举当日晚8点准时关闭。",
-      callout2: ""
+      "header": "邮寄投票箱（暂时关闭）",
+      "englishName": "Mail-in ballot dropbox (temporarily closed)",
+      "p1": "如果投票箱满了、故障或以任何形式损坏，请致电 (215) 686-3469 或发送电子邮件至 <a href='mailto:vote{'@'}phila.gov'>vote{'@'}phila.gov</a>.",
+      "h2": "",
+      "dates": "",
+      "callout1": "投票箱将于选举当日晚8点准时关闭。",
+      "callout2": ""
     }
   },
-  beforeYouGo: "在您出发前请先了解以下信息",
-  checkSite: "查看具体地点详情。",
-  hoursVary: "时间和可用性各不相同。",
-  eligibility: "详情",
-  testingHours: "检测时间",
-  daysOfOperation: "营业时间",
-  schedule: "安排",
-  Monday: "星期一",
-  Tuesday: "星期二",
-  Wednesday: "星期三",
-  Thursday: "星期四",
-  Friday: "星期五",
-  Saturday: "星期六",
-  Sunday: "星期日",
-  Yes: "是",
-  No: "否",
-  Unknown: "未知",
-  website: "网站",
-  closed: "休息"
+  "beforeYouGo": "在您出发前请先了解以下信息",
+  "checkSite": "查看具体地点详情。",
+  "hoursVary": "时间和可用性各不相同。",
+  "eligibility": "详情",
+  "testingHours": "检测时间",
+  "daysOfOperation": "营业时间",
+  "schedule": "安排",
+  "Monday": "星期一",
+  "Tuesday": "星期二",
+  "Wednesday": "星期三",
+  "Thursday": "星期四",
+  "Friday": "星期五",
+  "Saturday": "星期六",
+  "Sunday": "星期日",
+  "Yes": "是",
+  "No": "否",
+  "Unknown": "未知",
+  "website": "网站",
+  "closed": "休息"
 }
