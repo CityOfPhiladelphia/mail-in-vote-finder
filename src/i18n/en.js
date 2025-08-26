3d22b090553d733635ff89e0189e98c18fea8a0e
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-08-26 15:19:18 -0400

    makes project work with @phila/auto-translator-cli

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,125 +1,125 @@
-export default {
+export default{
   "language": "English",
-  app: {
-    title: "Election Offices and Mail-in Drop-off Locations",
-    subtitle: "Find a location near you.",
-    noResults: "No site was found within your search. Please try again."
+  "app": {
+    "title": "Election Offices and Mail-in Drop-off Locations",
+    "subtitle": "Find a location near you.",
+    "noResults": "No site was found within your search. Please try again."
   },
-  introPage: {
-    exclamation: "This map includes locations that accept mail-in and absentee ballots only. To vote in person, find your polling place at ",
-    introTitle: "About this finder",
-    p1_1: "The Philadelphia City Commissioners have approved ",
-    p1_2: " satellite election offices located throughout Philadelphia, in addition to the two existing County Board of Election offices.",
-    p2: "In a single visit to these locations, voters can:",
-    ul1: {
-      li1: "Register to vote,",
-      li2: "Request a mail-in ballot, and",
-      li3: "Receive their ballot, vote, and return it."
+  "introPage": {
+    "exclamation": "This map includes locations that accept mail-in and absentee ballots only. To vote in person, find your polling place at ",
+    "introTitle": "About this finder",
+    "p1_1": "The Philadelphia City Commissioners have approved ",
+    "p1_2": " satellite election offices located throughout Philadelphia, in addition to the two existing County Board of Election offices.",
+    "p2": "In a single visit to these locations, voters can:",
+    "ul1": {
+      "li1": "Register to vote,",
+      "li2": "Request a mail-in ballot, and",
+      "li3": "Receive their ballot, vote, and return it."
     },
-    p3: "Voters can also drop off their completed ballots at an election office.",
-    p4_b: "Note: ",
-    p4: "Voters must wait until October 6 to request a replacement ballot at the offices. This will prevent duplicating ballots already in the mail.",
-    relatedContent: "Related content",
-    link1: "Check your voter registration status",
-    link2: "Check the status of your mail-in ballot",
-    link3: "See our mail-in ballot guide for Philadelphia Voters"
+    "p3": "Voters can also drop off their completed ballots at an election office.",
+    "p4_b": "Note: ",
+    "p4": "Voters must wait until October 6 to request a replacement ballot at the offices. This will prevent duplicating ballots already in the mail.",
+    "relatedContent": "Related content",
+    "link1": "Check your voter registration status",
+    "link2": "Check the status of your mail-in ballot",
+    "link3": "See our mail-in ballot guide for Philadelphia Voters"
   },
-  cityOfPhiladelphia: "City of Philadelphia",
-  cityCommissioners: "City Commissioners",
-  ballotGuide: "General election mail-in ballot guide",
-  siteHours: "Site hours",
-  details: {
-    details: "Details",
-    inPerson: "In-person registration and mail-in voting",
-    voterReg: "Voter registration.",
-    ballotApp: "Mail-in ballot application",
-    ballotRep: "Mail-in ballot replacement",
-    ballotDropoff: "Mail-in ballot drop-off",
-    ballotDropoffMobile: "Mail-in mobile ballot drop-off",
-    ballotDropoffMobileStandAlone: "<div class='callout'>This is not a fixed, stand-alone dropbox</div>",
-    ballotDropoffLong: "The Board of Elections should be notified immediately in the event the receptacle is full, not functioning, or is damaged in any fashion, by calling 215-686-3469 or by emailing vote{'@'}phila.gov.",
-    interpretationAvailable: "Telephonic interpretation services available",
-    wheelchair: "Wheelchair accessible",
-    open24Hours: "Open 24 hours",
-    staff: "Staff will remain at the office to receive dropped off ballots until 8 p.m. Boxes will be locked at precisely 8 p.m.",
-    electionDayHours: "On Election Day, dropboxes will be locked at precisely 8 p.m."
+  "cityOfPhiladelphia": "City of Philadelphia",
+  "cityCommissioners": "City Commissioners",
+  "ballotGuide": "General election mail-in ballot guide",
+  "siteHours": "Site hours",
+  "details": {
+    "details": "Details",
+    "inPerson": "In-person registration and mail-in voting",
+    "voterReg": "Voter registration.",
+    "ballotApp": "Mail-in ballot application",
+    "ballotRep": "Mail-in ballot replacement",
+    "ballotDropoff": "Mail-in ballot drop-off",
+    "ballotDropoffMobile": "Mail-in mobile ballot drop-off",
+    "ballotDropoffMobileStandAlone": "<div class='callout'>This is not a fixed, stand-alone dropbox</div>",
+    "ballotDropoffLong": "The Board of Elections should be notified immediately in the event the receptacle is full, not functioning, or is damaged in any fashion, by calling 215-686-3469 or by emailing vote{'@'}phila.gov.",
+    "interpretationAvailable": "Telephonic interpretation services available",
+    "wheelchair": "Wheelchair accessible",
+    "open24Hours": "Open 24 hours",
+    "staff": "Staff will remain at the office to receive dropped off ballots until 8 p.m. Boxes will be locked at precisely 8 p.m.",
+    "electionDayHours": "On Election Day, dropboxes will be locked at precisely 8 p.m."
   },
   "Election office": "Election offices",
   "Official mobile mail-in ballot return": "Official mobile mail-in ballot return",
   "Official mail-in ballot dropbox": "Official mail-in ballot dropboxes",
-  categoryType: {
+  "categoryType": {
     "Election office": "Election offices",
     "Official mobile mail-in ballot return": "Official mobile mail-in ballot return",
     "Official mail-in ballot dropbox": "Official mail-in ballot dropboxes",
     "Mail-in ballot dropbox (temporarily closed)": "Mail-in ballot dropbox (temporarily closed)"
   },
-  sections: {
+  "sections": {
     "Election office": {
-      header: "Election offices",
-      englishName: "Election office",
-      p1: "Beginning October 28, 2025, the Philadelphia Board of Elections will be open for in-person mail-in ballot services for the November 4, 2025 Municipal Primary Election. Registered voters can apply for, receive, complete, and return their mail-in ballot in person in one visit.",
-      h2: "Voter services and deadlines ",
-      dates: {
-        d2:{
-          text: "Apply for a mail-in ballot",
-          date: "October 28 by 5:00 p.m."
+      "header": "Election offices",
+      "englishName": "Election office",
+      "p1": "Beginning October 28, 2025, the Philadelphia Board of Elections will be open for in-person mail-in ballot services for the November 4, 2025 Municipal Primary Election. Registered voters can apply for, receive, complete, and return their mail-in ballot in person in one visit.",
+      "h2": "Voter services and deadlines ",
+      "dates": {
+        "d2": {
+          "text": "Apply for a mail-in ballot",
+          "date": "October 28 by 5:00 p.m."
         },
-        d3:{
-          text: "Request a replacement ballot",
-          date: "TBD"
+        "d3": {
+          "text": "Request a replacement ballot",
+          "date": "TBD"
         },
-        d4:{
-          text: "Drop off your completed ballot",
-          date: "November 4, by 8:00 p.m."
+        "d4": {
+          "text": "Drop off your completed ballot",
+          "date": "November 4, by 8:00 p.m."
         }
       },
-      callout2: ""
+      "callout2": ""
     },
     "Official mobile mail-in ballot return": {
-      header: "Official mobile mail-in ballot return",
-      englishName: "Official mobile mail-in ballot return",
-      p1: "Mail-in mobile ballot drop-off.",
-      h2: "",
-      dates: "",
-      callout1: "This is not a fixed, stand-alone dropbox. Limited hours only.",
-      callout2: ""
+      "header": "Official mobile mail-in ballot return",
+      "englishName": "Official mobile mail-in ballot return",
+      "p1": "Mail-in mobile ballot drop-off.",
+      "h2": "",
+      "dates": "",
+      "callout1": "This is not a fixed, stand-alone dropbox. Limited hours only.",
+      "callout2": ""
     },
     "Official mail-in ballot dropbox": {
-      header: "Official mail-in ballot dropboxes",
-      englishName: "Official mail-in ballot dropbox",
-      p1: "If a dropbox is full, not functioning, or is damaged in any way, call (215) 686-3469 or email <a href='mailto:vote{'@'}phila.gov'>vote{'@'}phila.gov</a>.",
-      h2: "",
-      dates: "",
-      callout1: "On Election Day, dropboxes will be locked at precisely 8 p.m.",
-      callout2: ""
+      "header": "Official mail-in ballot dropboxes",
+      "englishName": "Official mail-in ballot dropbox",
+      "p1": "If a dropbox is full, not functioning, or is damaged in any way, call (215) 686-3469 or email <a href='mailto:vote{'@'}phila.gov'>vote{'@'}phila.gov</a>.",
+      "h2": "",
+      "dates": "",
+      "callout1": "On Election Day, dropboxes will be locked at precisely 8 p.m.",
+      "callout2": ""
     },
     "Mail-in ballot dropbox (temporarily closed)": {
-      header: "Mail-in ballot dropboxes (temporarily closed)",
-      englishName: "Mail-in ballot dropbox (temporarily closed)",
-      p1: "If a dropbox is full, not functioning, or is damaged in any way, call (215) 686-3469 or email <a href='mailto:vote{'@'}phila.gov'>vote{'@'}phila.gov</a>.",
-      h2: "",
-      dates: "",
-      callout1: "On Election Day, dropboxes will be locked at precisely 8 p.m.",
-      callout2: ""
+      "header": "Mail-in ballot dropboxes (temporarily closed)",
+      "englishName": "Mail-in ballot dropbox (temporarily closed)",
+      "p1": "If a dropbox is full, not functioning, or is damaged in any way, call (215) 686-3469 or email <a href='mailto:vote{'@'}phila.gov'>vote{'@'}phila.gov</a>.",
+      "h2": "",
+      "dates": "",
+      "callout1": "On Election Day, dropboxes will be locked at precisely 8 p.m.",
+      "callout2": ""
     }
   },
-  beforeYouGo: "Before you go",
-  checkSite: "Eligibility requirements and testing hours vary by site. Be sure to check site details to arrange for testing.",
-  hoursVary: "Hours and availability varies.",
-  eligibility: "Details",
-  testingHours: "Testing hours",
-  daysOfOperation: "Days of operation",
-  schedule: "Schedule",
-  Monday: "Monday",
-  Tuesday: "Tuesday",
-  Wednesday: "Wednesday",
-  Thursday: "Thursday",
-  Friday: "Friday",
-  Saturday: "Saturday",
-  Sunday: "Sunday",
-  Yes: "Yes",
-  No: "No",
-  Unknown: "Unknown",
-  website: "Website",
-  closed: "Closed"
+  "beforeYouGo": "Before you go",
+  "checkSite": "Eligibility requirements and testing hours vary by site. Be sure to check site details to arrange for testing.",
+  "hoursVary": "Hours and availability varies.",
+  "eligibility": "Details",
+  "testingHours": "Testing hours",
+  "daysOfOperation": "Days of operation",
+  "schedule": "Schedule",
+  "Monday": "Monday",
+  "Tuesday": "Tuesday",
+  "Wednesday": "Wednesday",
+  "Thursday": "Thursday",
+  "Friday": "Friday",
+  "Saturday": "Saturday",
+  "Sunday": "Sunday",
+  "Yes": "Yes",
+  "No": "No",
+  "Unknown": "Unknown",
+  "website": "Website",
+  "closed": "Closed"
 }
